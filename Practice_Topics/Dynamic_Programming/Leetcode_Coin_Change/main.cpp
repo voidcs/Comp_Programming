a82157c69955285d0faae9b28caa7dba99f850c2
James Rungsawang <47105784+voidcs@users.noreply.github.com>
2020-07-23 11:37:49 -0700

    Add files via upload

---
+++ Practice_Topics/Dynamic_Programming/Leetcode_Coin_Change/main.cpp
@@ -0,0 +1,26 @@
+
+//PROBLEM LINK
+//https://leetcode.com/problems/coin-change/
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        long long dp[amount+1];
+        long long INF = 10e10;
+        for(int i = 0; i <= amount; i++)
+            dp[i] = INF;
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(auto x: coins){
+                if(x <= i){
+                    dp[i] = min(dp[i], dp[i-x]+1);
+                }
+            }
+        }
+        if(dp[amount] == INF)
+            return -1;
+        return dp[amount];
+        
+    }
+};
