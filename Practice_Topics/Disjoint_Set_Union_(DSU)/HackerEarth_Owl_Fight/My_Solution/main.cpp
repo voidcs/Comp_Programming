093bd84d13a45356a01637c0a9404dd1202999d0
James Rungsawang <jamesrngsawang@gmail.com>
2020-08-25 16:08:29 -0700

    Owl fight dsu problem fixed
    Signed-off-by: James Rungsawang <jamesrngsawang@gmail.com>

--- Practice_Topics/Disjoint_Set_Union_(DSU)/HackerEarth_Owl_Fight/My_Solution/main.cpp
+++ Practice_Topics/Disjoint_Set_Union_(DSU)/HackerEarth_Owl_Fight/My_Solution/main.cpp
@@ -2,27 +2,66 @@
 using namespace std;
 typedef long long ll;
 
-int main() {
-    int n;
-    cin>>n;
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for(int i = 0; i < n; i++){
-        int x;
-        cin>>x;
-        pq.push(x);
+//PROBLEM LINK
+//https://www.hackerearth.com/practice/data-structures/disjoint-data-strutures/basics-of-disjoint-data-structures/practice-problems/algorithm/owl-fight/description/
+
+//REFERENCE VIDEO
+//https://www.youtube.com/watch?v=wq6TCikSzpk&list=PL2q4fbVm1Ik4JdzE2Bv_UUGBz0TXEIrai&index=7
+
+vector<int> par;
+vector<int> str;
+
+int find(int x){
+    return (par[x] == x) ? x: par[x] = find(par[x]);
+}
+
+void unite(int a, int b){
+    int x = find(a);
+    int y = find(b);
+
+    if(x != y){
+        par[x] = y;
+        str[y] = max(str[y], a);
+        str[y] = max(str[y], b);
+        str[y] = max(str[y], str[x]);
     }
-    
-    int need = 1;
-    int ans = 0;
-    for(int i = 0; i < n; i++){
-        int have = pq.top();
-        pq.pop();
-        if(have >= need){
-            ans++;
-            need++;
+}
+
+int main(){
+    //ios::sync_with_stdio(0);
+    //cin.tie(0);
+
+    int n, m;
+    cin>>n>>m;
+
+    str = vector<int>(n+5, 0);
+    par = vector<int>(n+5, 0);
+
+    iota(par.begin(), par.end(), 0);
+    iota(str.begin(), str.end(), 0);
+
+    int a, b;
+    while(m--){
+        cin>>a>>b;
+        unite(a, b);
+    }
+
+    int t;
+    cin>>t;
+    while(t--){
+        cin>>a>>b;
+        int x = find(a);
+        int y = find(b);
+        if(x == y){
+            cout<<"TIE\n";
+        }
+        else{
+            if(str[x] > str[y])
+                cout<<a<<endl;
+            else
+                cout<<b<<endl;
         }
     }
-    cout<<ans<<endl;
-    
+
     return 0;
-}
+} 
