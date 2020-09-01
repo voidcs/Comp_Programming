b749a206e723177cdb6f8d2f55ce05e634c8c732
James Rungsawang <jamesrngsawang@gmail.com>
2020-08-31 20:01:35 -0700

    Added two pointer topic + codeforces boats competition solution

---
+++ Practice_Topics/Two_Pointer/Codeforces_Boats_Competition/main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//PROBLEM LINK
+//https://codeforces.com/contest/1399/problem/C
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll a[n];
+        for(int i = 0; i < n; i++)
+            cin>>a[i];
+        
+        sort(a, a + n);
+        set<ll> sums;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                sums.insert(a[i] + a[j]);
+            }
+        }
+
+        ll ans = 0;
+        for(auto x: sums){
+            ll goal = x;
+
+            ll l = 0, r = n - 1;
+            ll count = 0;
+
+            while(l < r){
+                if(a[l] + a[r] > goal){
+                    r--;
+                }
+                else if(a[l] + a[r] < goal)
+                    l++;
+                else{
+                    count++;
+                    l++;
+                    r--;
+                }
+            }
+            ans = max(ans, count);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
