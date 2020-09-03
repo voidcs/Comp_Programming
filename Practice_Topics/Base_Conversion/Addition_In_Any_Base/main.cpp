c2bad51921ba4e67e9d9cb4a2a1c0725729d2519
James Rungsawang <jamesrngsawang@gmail.com>
2020-09-03 00:29:01 -0700

    Adding two numbers in any base calculator program

---
+++ Practice_Topics/Base_Conversion/Addition_In_Any_Base/main.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    
+    int base;
+    cout<<"Enter the base\n";
+    cin>>base;
+    string num1, num2;
+    cout<<"Enter the two numbers to be added\n";
+    cin>>num1>>num2;
+    
+    bool flag = 0;
+    vector<ll> n1, n2;
+    for(int i = 0; i < num1.length(); i++){
+        int x;
+        if(num1[i] <= '9'){
+            x = num1[i] - '0';
+        }
+        else{
+            x = 10;
+            x += num1[i] - 'A';
+        }
+        if(x >= base){
+            flag = 1;
+        }
+        n1.push_back(x);
+    }
+    
+    for(int i = 0; i < num2.length(); i++){
+        int x;
+        if(num2[i] <= '9'){
+            x = num2[i] - '0';
+        }
+        else{
+            x = 10;
+            x += num2[i] - 'A';
+        }
+        if(x >= base){
+            flag = 1;
+        }
+        n2.push_back(x);
+    }
+    
+    if(flag){
+        cout<<"INVALID NUMBER GIVEN THE BASE\n";
+        return 0;
+    }
+    
+//    Numbers
+//    for(auto x: n1)
+//        cout<<x<<" ";
+//    cout<<endl;
+//    
+//    for(auto x: n2)
+//        cout<<x<<" ";
+//    cout<<endl;
+    
+    reverse(n1.begin(), n1.end());
+    reverse(n2.begin(), n2.end());
+    
+    int m = min(n1.size(), n2.size());
+    
+    vector<ll> ans;
+    int carry = 0;
+    for(int i = 0; i < m; i++){
+        int x = n1[i];
+        int y = n2[i];
+        
+        int add = x + y + carry;
+        
+        if(add >= base){
+            add -= base;
+            carry = add%base;
+        }
+        
+        ans.push_back(add);
+    }
+    
+    int diff = max(n1.size(), n2.size()) - min(n1.size(), n2.size());
+    vector<ll> extra;
+    if(n1.size() > n2.size())
+        extra = n1;
+    else
+        extra = n2;
+    
+    for(int i = m; i < m + diff; i++){
+        ans.push_back(extra[i] + carry);
+        if(carry)
+            carry = 0;
+    }
+    
+    cout<<"ANS: ";
+    reverse(ans.begin(), ans.end());
+    for(auto x: ans){
+        char start = '0';
+        if(x >= 10){
+            start = 'A';
+            x -= 10;
+        }
+        start += x;
+        cout<<start;
+    }
+    
+    return 0;
+}
