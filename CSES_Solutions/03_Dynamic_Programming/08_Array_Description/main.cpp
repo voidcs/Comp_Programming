5f31c3fa9ebfb91b76df6ff8161a959181dcb052
James Rungsawang <jamesrngsawang@gmail.com>
2020-11-15 23:55:10 -0800

    CSES array description solution

---
+++ CSES_Solutions/03_Dynamic_Programming/08_Array_Description/main.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+const ll mod = 1e9 + 7;
+ 
+int main(){
+    ll n, m;
+    cin>>n>>m;
+    ll a[n];
+    for(int i = 0; i < n; i++){
+        cin>>a[i];
+        a[i]--;
+    }
+    
+    vector<vector<ll>> dp(n, vector<ll>(m, 0));
+    for(int i = 0; i < n; i++){
+        if(i){
+            for(int j = 0; j < m; j++){
+                dp[i][j] = dp[i-1][j];
+                if(j)
+                    dp[i][j] += dp[i-1][j-1];
+                if(j < m-1)
+                    dp[i][j] += dp[i-1][j+1];
+                dp[i][j] %= mod;
+            }
+        }
+        else{
+            for(int j = 0; j < m; j++)
+                dp[0][j] = 1;
+        }
+        if(~a[i]){
+            for(int j = 0; j < m; j++){
+                if(j != a[i])
+                    dp[i][j] = 0;
+            }
+        }
+    }
+    ll ans = 0;
+    for(int i = 0; i < m; i++)
+        ans += dp[n-1][i];
+    
+    cout<<ans%mod<<endl;
+    
+    return 0;
+}
