bf0066b9be10f5eef3e71ed3be3c0be1d96ed269
voidcs <jamesrngsawang@gmail.com>
2021-01-01 23:07:44 -0800

    Second solution to CSES problem using segment tree for practice

---
+++ CSES_Solutions/05_Range_Queries/01_Range_Sum_Queries_I/Segment_Tree/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1e18;
+ 
+int main(){
+    int n, q;
+    cin>>n>>q;
+    int a[n];
+    for(int i = 0; i < n; i++)
+        cin>>a[i];
+    vector<ll> tree(n*4);
+    
+    function<void(int, int, int)> build = [&](int si, int sl, int sr){
+        if(sl == sr){
+            tree[si] = a[sl];
+            return;
+        }
+        int mid = (sl+sr)/2;
+        build(si*2+1, sl, mid);
+        build(si*2+2, mid+1, sr);
+        tree[si] = tree[si*2+1] + tree[si*2+2];
+    };
+    int ql, qr;
+    function<ll(int, int, int)> query = [&](int si, int sl, int sr){
+        if(sl > qr || sr < ql)
+            return (ll)0;
+        if(sl >= ql && sr <= qr)
+            return tree[si];
+        int mid = (sl+sr)/2;
+        ll l = query(si*2+1, sl, mid);
+        ll r = query(si*2+2, mid+1, sr);
+        return l + r;
+    };
+    build(0, 0, n-1);
+    while(q--){
+        cin>>ql>>qr;
+        ql--, qr--;
+        cout<<query(0, 0, n-1)<<endl;
+    }
+    
+    return 0;
+}
