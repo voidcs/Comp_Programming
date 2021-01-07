2f0d45c1303273350475658b274bd82ac10dd277
voidcs <jamesrngsawang@gmail.com>
2021-01-07 02:41:46 -0800

    Tower of hanoi solution rewritten with lambda

--- CSES_Solutions/01_Introductory_Problems/14_Tower_Of_Hanoi/main.cpp
+++ CSES_Solutions/01_Introductory_Problems/14_Tower_Of_Hanoi/main.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-void solve(int n, int from, int to, int aux_rod) { 
-    if(n == 1){ 
-        cout<<from<<" "<<to<<endl;
-        return; 
-    } 
-    solve(n-1, from, aux_rod, to); 
-    cout<<from<<" "<<to<<endl;
-    solve(n-1, aux_rod, to, from); 
-} 
+typedef long long ll;
+const ll INF = 1e18;
  
 int main() { 
     int n;
     cin>>n;
     cout<<(1<<n)-1<<endl;
+    function<void(int, int, int, int)> solve = [&](int n, int from, int to, int aux){
+        if(n == 1){
+            cout<<from<<" "<<to<<endl;
+            return; 
+        }
+        solve(n-1, from, aux, to); 
+        cout<<from<<" "<<to<<endl;
+        solve(n-1, aux, to, from); 
+    };
     solve(n, 1, 3, 2);
     return 0; 
 } 
