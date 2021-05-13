8a76ad5f5a22cc07d91d40c91c8af6a373adfbd3
voidcs <jamesrngsawang@gmail.com>
2021-05-12 19:57:03 -0700

    Finally solved USACO stuck in a rut problem

---
+++ USACO_Guide/Bronze/01_Getting_Started/04_Simulation/Stuck_In_A_Rut/main.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1e9+7;
+const ll INF = 1e18;
+
+struct cow{
+    ll x, y, id;
+    char d;
+    bool operator < (const cow &a){
+        return y < a.y;
+    }
+};
+
+int main(){
+    //freopen("input.txt", "r", stdin);
+    int n;
+    cin>>n;
+    vector<cow> v(n);
+    for(int i = 0; i < n; i++){
+        cin>>v[i].d>>v[i].x>>v[i].y;
+        v[i].id = i;
+    }
+    vector<array<cow, 2>> intersections;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(v[i].d == v[j].d)
+                continue;
+            //Make a the cow going north
+            cow a = v[i], b = v[j];
+            if(a.d == 'E')
+                swap(a, b);
+            if(a.x > b.x && b.y > a.y)
+                intersections.push_back({a, b});
+        }
+    }
+    //We first process the intersection where a cow would be stopped as quickly as possible
+    auto comp = [&](array<cow, 2> a, array<cow, 2> b){
+        //The cow with furthest distance is the one that gets stopped
+        ll distA = max(a[0].x - a[1].x, a[1].y - a[0].y);
+        ll distB = max(b[0].x - b[1].x, b[1].y - b[0].y);
+        //Take the min between both to see which intersection has a cow stopping earlier
+        return distA < distB;
+    };
+    sort(intersections.begin(), intersections.end(), comp);
+    vector<ll> stopped(n, -1);
+    for(array<cow, 2> x: intersections){
+        cow a = x[0], b = x[1];
+        //If both cows are stopped we do nothing
+        if(stopped[a.id] != -1 && stopped[b.id] != -1)
+            continue;
+        //If both cows haven't been stopped we find their intersection point
+        //and then see which one would be stopped
+        if(stopped[a.id] == -1 && stopped[b.id] == -1){
+            if(abs(a.x - b.x) != abs(a.y - b.y)){
+                if(b.y - a.y < a.x - b.x)
+                    stopped[b.id] = a.x - b.x;
+                else
+                    stopped[a.id] = b.y - a.y;
+            }
+        }
+        else{
+            //If cow B has already stopped and cow A hasn't
+            if(stopped[b.id] != -1){
+                ll x = b.x + stopped[b.id], y = b.y;
+                if(x >= a.x){
+                    stopped[a.id] = y - a.y;
+                }
+            }
+            //Otherwise if cow A has stopped and cow B hasn't
+            else{
+                ll x = a.x, y = a.y + stopped[a.id];
+                if(y >= b.y){
+                    stopped[b.id] = x - b.x;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        if(stopped[i] == -1)
+            cout<<"Infinity\n";
+        else
+            cout<<stopped[i]<<endl;
+    }
+    
+    return 0;
+}
