dd83486d587a5349934857883698dd4f18afe95c
voidcs <jamesrngsawang@gmail.com>
2021-06-06 22:39:21 -0700

    Where am I USACO solution

---
+++ USACO_Guide/Bronze/03_Sorting_And_Sets/02_Introduction_to_Sets_and_Maps/Where_Am_I/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1e9+7;
+const ll INF = 1e18;
+
+int main(){
+    freopen("whereami.in", "r", stdin);
+    freopen("whereami.out", "w", stdout);
+    int n;
+    string s;
+    cin>>n>>s;
+    for(int i = 2; i < n; i++){
+        bool valid = 1;
+        set<string> have;
+        for(int j = 0; j < n-i+1; j++){
+            string x = s.substr(j, i);
+            if(have.count(x))
+                valid = 0;
+            have.insert(x);
+        }
+        if(valid){
+            cout<<i<<endl;
+            return 0;
+        }
+    }
+    
+    return 0;
+}
