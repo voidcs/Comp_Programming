367cbb6eebcfd94f0bc4f6904473423b9ea3cd34
voidcs <jamesrngsawang@gmail.com>
2021-09-07 08:58:16 -0700

    Updated decimal to any base

--- Practice_Topics/Base_Conversion/Decimal_to_any/Version_2.cpp
+++ Practice_Topics/Base_Conversion/Decimal_to_any/Version_2.cpp
@@ -3,23 +3,16 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
-    ll x;
-    ll base;
+    ll x, base;
     cout<<"Enter the base 10 number to be converted\n";
     cin>>x;
-
-    
     cout<<"Enter the base to convert to\n";
     cin>>base;
     
     vector<char> v;
-    
     cout<<x<<" in base: "<<base<<" is: ";
     while(x){
-        ll add = 0;
         char start = '0';
-        
         int r = x%base;
         if(r > 9){
             start = 'A';
